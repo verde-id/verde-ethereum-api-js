36aa7b143d47baa71ffdb0866c6234f2dcaf0f9d
Raphassit Leelachaikul <raphassitlee@gmail.com>
2018-02-23 15:35:00 +0700

    Change method name; Add 'verde-ethereum' as dependency; Add babel config file;

--- src/idp.js
+++ src/idp.js
@@ -6,7 +6,7 @@ export const registerAuthenticationRequestCallbackUrl = (callbackUrl) => {
 
 };
 
-export const getAuthenticationResponse = () => {
+export const authenticationResponse = () => {
 
 };
 
